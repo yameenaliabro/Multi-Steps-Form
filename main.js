7ea801ac737c8a43659c4f662c99299d4eccadf8
Muhammad Yameen <106884997+yameenaliabro@users.noreply.github.com>
2023-10-30 23:20:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,337 @@
+const Berlin = [
+    {
+        name: "Beratungsgespräch",
+        duration: "30 Minuten",
+        price: "€ 0,00"
+    },
+    {
+        name: "Beratungsgespräch Lidstrich oder Wimpernkranzverdichtung",
+        duration: "30 Minuten",
+        price: "€ 0,00"
+    },
+    {
+        name: "Beratungsgespräch Lippen",
+        duration: "30 Minuten",
+        price: "€ 0,00"
+    },
+    {
+        name: "Beratungsgespräch mehrerer Behandlungsareale (Beauty)",
+        duration: "45 Minuten",
+        price: "€ 0,00"
+    },
+    {
+        name: "Beratungsgespräch Medizinische Pigmentierung (z.B. Narben, Pigmentflecken etc.)",
+        duration: "30 Minuten",
+        price: "€ 0,00"
+    },
+    {
+        name: "Beratungsgespräch Micro Scalp Pigmentation (Kopfhaut Pigementierung)",
+        duration: "30 Minuten",
+        price: "€ 0,00"
+    },
+]
+const Augenbrauen = [
+    {
+        name: "Augenbrauen Neupigmentierung",
+        duration: "90 Minuten",
+        price: "€ 650,00"
+    },
+    {
+        name: "Augenbrauen erste Nachbehandlung (Zweite Sitzung)",
+        duration: "60 Minuten",
+        price: "€ 0,00"
+    },
+    {
+        name: "Augenbrauen zweite Nachbehandlung (Dritte Sitzung)",
+        duration: "30 Minuten",
+        price: "€ 0,00"
+    },
+    {
+        name: "Augenbrauen Auffrischungspigmentierung bis zu 1 Jahr",
+        duration: "60 Minuten",
+        price: "€ 250,00"
+    },
+    {
+        name: "Augenbrauen Auffrischungspigmentierung bis zu 2 Jahren",
+        duration: "60 Minuten",
+        price: "€ 400,00"
+    },
+    {
+        name: "Augenbrauen Auffrischungspigmentierung bis zu 3 Jahren",
+        duration: "60 Minuten",
+        price: "€ 650,00"
+    },
+    {
+        name: "Augenbrauen Pigmentierung bei bereits vorhandener Fremdpigmentierung (evtl.Retusche) Gesamtpreis nach Absprache vor Ort",
+        duration: "60 Minuten",
+        price: "€ 0,00"
+    },
+    {
+        name: "Augenbrauen Pigmentierung 1/2 jährlich",
+        duration: "45 Minuten",
+        price: "€ 150,00"
+    },
+    // Add more service objects for step 1
+];
+
+const Wimpernkranz = [
+    {
+        name: "Wimpernkranzverdichtung Oben neu (keine durchgängige Line)",
+        duration: "60 Minutes — Bei der Wimpernkranzverdichtung wird auf Wunsch der natürliche Wimpernkranzbereich ganz minimalistisch anschattiert, sodass keine durchgängige Linie sichtbar ist. Dieser wird eher bei Männern, oder bei Damen ohne eigenem Naturhaar angelegt.",
+        price: "€ 350,00"
+    },
+    {
+        name: "Wimpernkranzverdichtung Unten neu (keine durchgängige Line)",
+        duration: "45 Minutes — Bei der Wimpernkranzverdichtung wird auf Wunsch der natürliche Wimpernkranzbereich ganz minimalistisch anschattiert, sodass keine durchgängige Linie sichtbar ist. Dieser wird eher bei Männern, oder bei Damen ohne eigenem Naturhaar angelegt.",
+        price: "€ 250,00"
+    },
+    {
+        name: "Wimpernkranzverdichtung Oben, Erste Nachbehandlung (Zweite Sitzung)",
+        duration: "45 Minutes",
+        price: "€ 0,00"
+    },
+    {
+        name: "Wimpernkranzverdichtung Unten, Erste Nachbehandlung (Zweite Sitzung)",
+        duration: "30 Minutes",
+        price: "€ 0,00"
+    },
+    {
+        name: "Wimpernkranz Auffrischungspigmentierung oben innerhalb zwei Jahren",
+        duration: "60 Minutes",
+        price: "€ 275,00"
+    },
+    {
+        name: "Wimpernkranz Auffrischungspigmentierung unten innerhalb zwei Jahren",
+        duration: "45 Minutes",
+        price: "€ 200,00"
+    },
+    {
+        name: "Wimpernkranz Pigmentierung bei bereits vorhandener Fremdpigmentierung (evtl.Retusche) Gesamtpreis nach Absprache vor Ort",
+        duration: "60 Minutes",
+        price: "€ 0,00"
+    },
+]
+
+const Lidstrich = [
+    {
+        name: "Lidstrich Pigmentierung oben neu (dezent bis mittelstark)",
+        duration: "60 Minutes",
+        price: "€ 450,00"
+    },
+    {
+        name: "Lidstrich oben erste Nachbehandlungspigmentierung (Zweite Sitzung)",
+        duration: "60 Minutes",
+        price: "€ 0,00"
+    },
+    {
+        name: "Lidstrich oben zweite Nachbehandlungspigmentierung (Dritte Sitzung)",
+        duration: "45 Minutes",
+        price: "€ 0,00"
+    },
+    {
+        name: "Lidstrich Pigmentierung oben neu (dekorativ)",
+        duration: "90 Minutes",
+        price: "€ 550,00"
+    },
+    {
+        name: "Lidstrich Pigmentierung unten neu (dezent bis mittelstark)",
+        duration: "60 Minutes",
+        price: "€ 350,00"
+    },
+    {
+        name: "Lidstrich unten erste Nachbehandlungspigmentierung (Zweite Sitzung)",
+        duration: "30 Minutes",
+        price: "€ 0,00"
+    },
+    {
+        name: "Lidstrich unten zweite Nachbehandlungspigmentierung (Dritte Sitzung)",
+        duration: "30 Minutes",
+        price: "€ 0,00"
+    },
+    {
+        name: "Lidstrich Pigmentierung unten neu (sehr dekorativ)",
+        duration: "60 Minutes",
+        price: "€ 450,00"
+    },
+    {
+        name: "Lidstrich oben Auffrischungspigmentierung bis zu zwei Jahren",
+        duration: "60 Minutes",
+        price: "€ 300,00"
+    },
+    {
+        name: "Lidstrich unten Auffrischungspigmentierung bis zu zwei Jahren",
+        duration: "45 Minutes",
+        price: "€ 250,00"
+    },
+    {
+        name: "Lidstrich Pigmentierung bei bereits vorhandener Fremdpigmentierung (evtl.Retusche) Gesamtpreis nach Absprache vor Ort",
+        duration: "60 Minutes",
+        price: "€ 0,00"
+    },
+]
+const Retusch = [
+    {
+        name: "Retuschierungspigmentierung (1 Stunde)",
+        duration: "60 Minutes",
+        price: "€ 250,00"
+    },
+    {
+        name: "Retuschierungspigmentierung (2 Stunden)",
+        duration: "120 Minutes",
+        price: "€ 500,00"
+    },
+]
+const Lippen = [
+    {
+        name: "Lippen Neupigmentierung (Wahlweise Kontur mit Teil- oder Vollschattierung/Lip Blush/Ombré Lips",
+        duration: "120 Minuten — Hierbei wird auf Wunsch ausgehend von der symmetrisch angelegten Kontur eine Teil oder Vollschattierung des Mundes gewählt mit fließendem Übergang und das natürlicher Lippenrot des eigenen Mundes.",
+        price: "€ 1.150,00"
+    },
+    {
+        name: "Lippen Neupigmentierung (bei wesentlich volleren Lippen oder einer sehr starken Vergrößerung)",
+        duration: "120 Minutes",
+        price: "€ 1.350,00"
+    },
+    {
+        name: "Lippenpigmentierung, Erste Nachbehandlung (Zweite Sitzung) zur Fertigstellung",
+        duration: "90 Minutes",
+        price: "€ 0,00"
+    },
+    {
+        name: "Lippenpigmentierung, Zweite Nachbehandlung (Dritte Sitzung) zur Fertigstellung",
+        duration: "60 Minutes",
+        price: "€ 0,00"
+    },
+    {
+        name: "Lippen Auffrischungspigmentierung innerhalb eines Jahres ohne Nachbehandlungen",
+        duration: "90 Minutes",
+        price: "€ 400,00"
+    },
+    {
+        name: "Lippen Auffrischungspigmentierung innerhalb zwei Jahren inkl. evtl. Nachbehandlung",
+        duration: "90 Minutes",
+        price: "€ 650,00"
+    },
+    {
+        name: "Liplight, auf Wunsch im Anschluss der fertigen Lippenpigmentierung (Ein nachträgliches Highlight mit einem hautfarbenen Pigment)",
+        duration: "60 Minutes — Beim Setzen des Lip Lights wird mit Hilfe eines hautfarbenen Tons der Mund um die bereits pigmentierte Kontur visuell betont. Erwirkt etwas dreidimensionaler",
+        price: "€ 150,00"
+    },
+    {
+        name: "Lippen Pigmentierung bei bereits vorhandener Fremdpigmentierung (evtl.Retusche) Gesamtpreis nach Absprache vor Ort",
+        duration: "120 Minutes",
+        price: "€ 0,00"
+    }
+]
+// You can keep adding more objects to the servicesData array as needed.
+
+// Combine data from different steps into a single array
+const dataByStep = {
+    1: [...Berlin, ...Augenbrauen, ...Wimpernkranz, ...Lidstrich, ...Retusch, ...Lippen],
+};
+
+let currentStep = 1;
+
+function createServiceElements(step) {
+    const servicesContainer = document.getElementById("services-container");
+    servicesContainer.innerHTML = ""; // Clear previous data
+
+    const data = dataByStep[step] || [];
+
+    data.forEach(service => {
+        const serviceDiv = document.createElement("div");
+        serviceDiv.className = "servce-row";
+        serviceDiv.innerHTML = `
+                                                                                <div class="servce-inner">
+                                                                                    <div class="inner-check">
+                                                                                        <input type="checkbox" class="checkbox-servce" id="${service.name}" />
+                                                                                        <label for="${service.name}" class="service-label">${service.name}</label>
+                                                                                    </div>
+                                                                                    <div class="service-price-duration">
+                                                                                        <p class="service-price">${service.price}</p>
+                                                                                    </div>
+                                                                                </div>
+                                                                                <div class="servce-duration-para">
+                                                                                    <p>${service.duration}</p>
+                                                                                </div>
+                                                                            `;
+        servicesContainer.appendChild(serviceDiv);
+    });
+}
+createServiceElements(currentStep);
+document.addEventListener('DOMContentLoaded', function () {
+    createServiceElements(currentStep);
+
+    const buttons = document.querySelectorAll('.button1');
+    const checkboxes = document.querySelectorAll('.checkbox-servce');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            // Remove the "active-button" class from all buttons
+            buttons.forEach(btn => btn.classList.remove('active-button'));
+
+            // Add the "active-button" class to the clicked button
+            this.classList.add('active-button');
+
+            // Check if any checkbox is checked
+            const isChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+
+            // Show or hide the popup based on checkbox status
+            const popup = document.getElementById('popup');
+            if (isChecked) {
+                popup.style.display = 'block';
+            } else {
+                popup.style.display = 'none';
+            }
+        });
+    });
+
+    // Add event listeners for checkboxes
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', function () {
+            // Check if any checkbox is checked
+            const isChecked = Array.from(checkboxes).some(checkbox => checkbox.checked);
+
+            // Show or hide the popup based on checkbox status
+            const popup = document.getElementById('popup');
+            if (isChecked) {
+                popup.style.display = 'block';
+            } else {
+                popup.style.display = 'none';
+            }
+        });
+    });
+});
+// Get the input element and services
+const searchInput = document.getElementById('service-search');
+const services = document.querySelectorAll('.servce-row');
+// Add an input event listener to the search input
+searchInput.addEventListener("input", function () {
+    const searchValue = searchInput.value.toLowerCase();
+    console.log("🚀 ~ file: main.js:321 ~ searchValue:", searchValue)
+
+    services.forEach(service => {
+        const serviceName = service.querySelector('.service-label').textContent.toLowerCase();
+        // Check if the service name includes the search value
+        if (serviceName.includes(searchValue)) {
+            service.style.display = 'block'; // Show the service
+        } else {
+            service.style.display = 'none'; // Hide the service
+        }
+    });
+});
+
+
+function nextStep(step) {
+    if (step === currentStep) {
+        if (step < 3) {
+            document.querySelector(`.step:nth-child(${step})`).classList.remove("active");
+            currentStep++;
+            document.querySelector(`.step:nth-child(${currentStep})`).classList.add("active");
+        } else {
+            // Handle form submission here
+            document.querySelector(`.step:nth-child(${currentStep})`).classList.remove("active");
+            document.getElementById("result").classList.add("active");
+        }
+    }
+}
